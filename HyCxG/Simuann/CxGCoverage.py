e81f1709289afaf7d26122de1dc6631f245e9c79
Xlxw <xlxw@xlxw.org>
2023-05-25 16:12:53 +0800

    Update examples for Cond-MC

--- HyCxG/Simuann/CxGCoverage.py
+++ HyCxG/Simuann/CxGCoverage.py
@@ -1,5 +1,7 @@
 from Simuann.SimuAnneal import Annealer
 import random
+import operator
+from functools import reduce
 
 PATTERN_SCORE = {
     'WORD' : 1.5,
@@ -10,7 +12,8 @@ PATTERN_SCORE = {
 
 COVERAGE_SCORE = {
     'COVER' : 1,
-    'INTER' : -0.4
+    'INTER' : -0.4,
+    'ABSTRACT': 0.3
 }
 THRESHOLD_RATE = 1000
 
@@ -43,41 +46,40 @@ class CxGCoverageProblem(Annealer):
         self.starts = starts
         self.ends = ends
         self.pattern_score = self.calculate_pattern_score(patterns)
-        self.max_coverage = max(ends) - min(starts)
+        self.max_coverage = len(set(reduce(operator.add, [list(range(starts[i], ends[i])) for i in range(len(patterns))])))
+        self.maximum_score = self.max_coverage * COVERAGE_SCORE['COVER'] + COVERAGE_SCORE['ABSTRACT'] * PATTERN_SCORE['WORD']
 
     def flip(self):
         ids = random.randint(0, len(self.cxgs)-1)
         self.state[ids] = 1- self.state[ids]
-        if max(self.state) < 1:
-            ids = random.randint(0, len(self.cxgs)-1)
-            self.state[ids] = 1 - self.state[ids]
+        if max(self.state) < 1: self.state[random.randint(0, len(self.cxgs)-1)] = 1 - self.state[ids]
 
     def move(self):
         initial_energy = self.energy()
         self.flip()
         self.watch_dog += 1
-        if self.watch_dog / (2 ** len(self.cxgs)) <= THRESHOLD_RATE:
-            return self.energy() - initial_energy
-        else:
-            return self.energy() - initial_energy + THRESHOLD_RATE
+        current_energy = self.energy()
+        if len(self.cxgs) == 2 and self.watch_dog / (2 ** len(self.cxgs)) <= THRESHOLD_RATE: return current_energy - initial_energy + THRESHOLD_RATE
+        else: return current_energy - initial_energy
 
     def energy(self):
-        maximum_score = self.max_coverage * COVERAGE_SCORE['COVER'] + PATTERN_SCORE['WORD']
-        e, pattern_score, coverage = 0, 0., []
+        pattern_score, coverage, intersect = 0., [], []
         for i in range(len(self.cxgs)):
             if self.state[i] == 1:
-                coverage.extend(list(range(self.starts[i], self.ends[i])))
+                for j in range(self.starts[i], self.ends[i]):
+                    if j not in coverage: coverage.append(j)
+                    elif j in coverage and j not in intersect: intersect.append(j)
                 pattern_score += self.pattern_score[i]
-        pattern_score /= sum(self.state)
-        set_cover = set(coverage)
-        e += (len(set_cover) * COVERAGE_SCORE['COVER'] + (len(coverage) - len(set_cover)) * COVERAGE_SCORE['INTER']) + pattern_score
-        return maximum_score - e
+        pattern_score= pattern_score / len(self.cxgs)
+        e = len(coverage) * COVERAGE_SCORE['COVER'] + len(intersect) * COVERAGE_SCORE['INTER'] + COVERAGE_SCORE['ABSTRACT'] * pattern_score
+        return self.maximum_score - e
 
     def calculate_pattern_score(self, patterns):
         pattern_scores = []
         for pat in patterns:
             scores = [obtain_elescore(p) for p in pat.split('--')]
             scores = sum([sco[1] for sco in scores]) / sum([sco[0] for sco in scores])
+            # scores = sum([sco[1] for sco in scores])
             pattern_scores.append(scores)
         return pattern_scores
 
@@ -89,6 +91,14 @@ if __name__ == '__main__':
     #     'the--NOUN--was' : (1, 4),
     #     'NOUN--AUX--ADV' : (2, 5),
     #     'AUX--so--ADJ' : (3, 6)
+    # }
+
+    # cxgs = {
+    #     'the--NOUN--is': (0, 3),
+    #     'the--NOUN--AUX--not': (0, 4),
+    #     'NOUN--is--not': (1, 4),
+    #     'their--NOUN--and': (8, 11),
+    #     'VERB--up--ADP': (13, 16)
     # }
 
     # Long Example
@@ -112,15 +122,14 @@ if __name__ == '__main__':
     #     'other--NOUN--of' : (28, 31)
     # }
 
-    # Example
     cxgs = {
         'NOUN--PUNCT--NOUN': (9, 12),
         'the--NOUN--are': (14, 17),
-        'enough--to--VERB--DET--NOUN': (18, 23), #
+        'enough--to--VERB--DET--NOUN': (18, 23),
         'enough--PART--VERB': (18, 21),
         'to--VERB--DET--NOUN--in': (19, 24),
         'to--VERB--DET--NOUN': (19, 23),
-        'to--VERB--DET': (19, 22), #
+        'to--VERB--DET': (19, 22),
         'VERB--DET--NOUN': (20, 23),
         'VERB--DET--NOUN--ADP': (20, 24),
         'the--NOUN--ADP': (21, 24),
@@ -139,6 +148,7 @@ if __name__ == '__main__':
         'VERB--DET--NOUN--[2]': (29, 32),
         'NOUN--PUNCT--NOUN--[2]': (31, 34)
     }
+
     cxg_names = list(cxgs)
     init_state = [0] * len(cxg_names)
     for _ in range(random.randint(1, len(cxg_names))):
